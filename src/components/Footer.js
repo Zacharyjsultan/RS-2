401716aabd3c0afa8631219043c0f7355f2c2834
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2023-03-02 18:07:22 -0800

    css work on contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,36 +2,34 @@ import React from "react";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import "./Footer.css";
+import Button from "@mui/material/Button";
 
 export default function Footer() {
   return (
     <div className="footer-container">
-      <div className="footer-text">sdfsdfsd</div>
+      <div className="instagram">sdfsdf</div>
       <a
         className="Email"
         href="mailto:RebeccaSultan@gmail.com"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <button
+        <Button
           variant="contained"
           onClick={() => {
             alert("this will take your email");
           }}
         >
           Email
-        </button>
-      </a>
-      <div className="footer">
-        {" "}
-        <Stack direction="column" spacing={2}>
-          <Avatar
-            alt="little rs"
-            className="avatar"
-            src={`${process.env.PUBLIC_URL}/RS.png`}
-          />
-        </Stack>
-      </div>
+        </Button>
+      </a>{" "}
+      <Stack direction="row" spacing={2}>
+        <Avatar
+          alt="little zach"
+          className="avatarr"
+          src={`${process.env.PUBLIC_URL}/RS.png`}
+        />
+      </Stack>
     </div>
   );
 }
